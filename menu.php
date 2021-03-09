2ed6df7a76cc24620c4c6285ea9c622610e286b0
rika25fgfg <rika25greeeen777@gmail.com>
2021-03-09 12:38:55 +0900

    メニュー画面

---
+++ menu.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>メニュー画面</title>
+    <h2>食べ放題コース一覧</h2>
+    <h3>【食べ放題】♦♢DXコース♢♦3500円(税込)</h3>
+    <img src="images/meat1.jpg">
+    <h5>デザートも付いた、当店メニューを食べ尽くせる贅沢なコースです♪</h5>
+    <h5>コース品数:63品/利用人数:1名～</h5>
+    <h1>3,500円（税込）</h1>
+    <h3>【食べ放題】♦♢シンプルコース♢♦2800円(税込)</h3>
+    <img src="images/meat2.jpg">
+    <h5>当店オススメ!!看板メニューを多数含む、最もスタンダードなコースです。</h5>
+    <h5>コース品数:42品/利用人数:1名～</h5>
+    <h1>2,800円（税込）</h1>
+    <h3>【食べ放題】♦♢満腹コース♢♦1900円(税込)</h3>
+    <img src="images/meat3.jpg">
+    <h5>リーズナブルが嬉しい◎</h5>
+    <h5>コース品数:21品/利用人数:1名～</h5>
+    <h1>1,900円（税込）</h1>
+
+
+</head>
+<body>
+    <pre>
+    
+    </pre>
+</body>
+</html>
---
+++ show.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>メニュー詳細</title>
+    <h2>メニュー詳細</h2>
+    <h3>【食べ放題】♦♢DXコース♢♦3500円(税込)</h3>
+    <img src="images/meat1.jpg">
+    <h3>コース内容</h3>
+    <h5>♢♦♢♦肉メニュー♢♦♢♦<br>
+    牛タン<br>
+    牛上トロタン<br>
+    中落カルビ(タレ・塩)<br>
+    牛ミノ(塩・にんにく塩・激辛)<br>
+    <p></p>
+    ♢♦♢♦サイドメニュー♢♦♢♦♢<br>
+    プチ紋甲イカ/有頭エビ/イイダコ<br>
+    ビビンバ
+    サンチュ/チョレギサラダ/シーザーサラダ/安安サラダ
+    韓国のり/梅干し/薬味ネギ<br>
+    バターフォンデュ/
+    シューアイス/生チョコアイス/ロイヤルミルクティーアイス/バニラアイス
+    </h5>
+    
+
+</head>
+<body>
+    
+</body>
+</html>

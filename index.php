baeba2a7695f1e9fb328d9496f8078e9b8e2d972
okada <shugo140208@softbank.ne.jp>
2021-03-09 12:48:21 +0900

    岡田

--- index.php
+++ index.php
@@ -1,1 +1,32 @@
-<?php phpinfo(); ?>
+<?php
+
+function h($s){
+  return htmlspecialchars($s, ENT_QUOTES, 'utf-8');
+}
+
+session_start();
+if (isset($_SESSION['EMAIL'])) {
+  echo 'ようこそ' .  h($_SESSION['EMAIL']) . "さん<br>";
+  echo "<a href='/logout.php'>ログアウトはこちら。</a>";
+  exit;
+}
+
+ ?>
+
+<!DOCTYPE html>
+<html lang="ja">
+ <head>
+   <meta charset="utf-8">
+   <title>Login</title>
+ </head>
+ <body>
+   <h1>ようこそ、ログインしてください。</h1>
+   <form  action="login.php" method="post">
+     <label for="email">ユーザー名</label>
+     <input type="email" name="email">
+     <label for="password">パスワード</label>
+     <input type="password" name="password">
+     <button type="submit">サインイン</button>
+   </form>
+ </body>
+</html>

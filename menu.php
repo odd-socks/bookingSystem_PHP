93aa8e7add8b59f60c24080298da0da87ef1a3e9
rika25fgfg <rika25greeeen777@gmail.com>
2021-03-10 12:39:26 +0900

    メニュー

--- menu.php
+++ menu.php
@@ -5,28 +5,32 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>メニュー画面</title>
+    </head>
+    <body>
+    <center>
+    
     <h2>食べ放題コース一覧</h2>
-    <h3>【食べ放題】♦♢DXコース♢♦3500円(税込)</h3>
+    <div class="box3">
+    <a href="show.php#1"><h4>【食べ放題】♦♢DXコース♢♦3500円(税込)</h4></a>
     <img src="images/meat1.jpg">
     <h5>デザートも付いた、当店メニューを食べ尽くせる贅沢なコースです♪</h5>
     <h5>コース品数:63品/利用人数:1名～</h5>
     <h1>3,500円（税込）</h1>
-    <h3>【食べ放題】♦♢シンプルコース♢♦2800円(税込)</h3>
-    <img src="images/meat2.jpg">
+
+    <a href="show.php#2"><h4>【食べ放題】♦♢満腹コース♢♦2800円(税込)</h4></a>
+    <div class="img2"><img src="images/meat2.jpg"></div>
     <h5>当店オススメ!!看板メニューを多数含む、最もスタンダードなコースです。</h5>
     <h5>コース品数:42品/利用人数:1名～</h5>
     <h1>2,800円（税込）</h1>
-    <h3>【食べ放題】♦♢満腹コース♢♦1900円(税込)</h3>
+    <a href="show.php#3"><h4>【食べ放題】♦♢シンプルコース♢♦1900円(税込)</h4></a>
     <img src="images/meat3.jpg">
     <h5>リーズナブルが嬉しい◎</h5>
     <h5>コース品数:21品/利用人数:1名～</h5>
     <h1>1,900円（税込）</h1>
+    </center>
 
 
-</head>
-<body>
-    <pre>
-    
-    </pre>
+
 </body>
 </html>
+

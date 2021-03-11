d7db367f8782f923fb13120db9a9f05b5f3a9267
Naoki <freeadores@gmail.com>
2021-03-11 09:57:02 +0900

    データベース接続の設定ファイルを作成

--- index.php
+++ index.php
@@ -1,1 +1,45 @@
-<?php phpinfo(); ?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title肉</title>
+</head>
+<body>
+  <h1>焼肉屋です。</h1>
+  <header>
+    <nav>
+      <ul>
+        <li>トップ</li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+    </nav>
+  </header>
+
+
+<?php
+	require 'db_connect.php';
+	//SQL文を作る（プレースホルダを使った式）
+	$sql = "select * from user ";
+	//プリペアードステートメントを作る
+	$stm = $pdo->prepare($sql);
+	//SQL文を実行する
+	$stm->execute();
+	//結果の取得（連想配列で受け取る）
+	$result = $stm->fetchAll(PDO::FETCH_ASSOC);
+  echo '<ul>';
+  foreach ($result as $row) {
+    echo " <li>{$row['name']}</li>";
+  }
+  // print_r($result);
+echo '</ul>'
+?>
+
+
+
+
+</body>
+</html>

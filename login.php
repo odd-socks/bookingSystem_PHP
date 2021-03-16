d0d2a4d23a6d82d66fc2553b336a62a854da51a6
okada <shugo140208@softbank.ne.jp>
2021-03-16 10:32:55 +0900

    お知らせ+実装

--- login.php
+++ login.php
@@ -9,13 +9,12 @@
 </head>
 
 <body>
-    <form action="login_comp.php" method="post">
-        <h1>ログイン名</h1>
-          <input type="text" size="35" name="login"><br>
-        <h1>パスワード
-          <input type="password" size="35" name="password"></h1><br>
-          <input type="submit" value="ログイン">
-    </form>
+    <form action="login_comp.php" method="post"></form>
+        <h1>ログイン名
+          <input type="text" size="35" name="login"></h1><br>
+        <h2>パスワード
+          <input type="password" size="35" name="password"></h2><br>
+        <h3><input type="submit" value="ログイン"></h3>
 </body>
 
 </html>

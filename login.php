040219fdd67593d46648c050f31d05ac806442be
okada <shugo140208@softbank.ne.jp>
2021-03-16 12:42:16 +0900

    お知らせ編集

--- login.php
+++ login.php
@@ -5,16 +5,31 @@
 <head>
     <meta charset="UTF-8">
     <title>ログイン画面</title>
-    <link rel="stylesheet" href="okada.css">
+    <link rel="stylesheet" href="css/okada.css">
 </head>
 
 <body>
-    <form action="login_comp.php" method="post"></form>
-        <h1>ログイン名
-          <input type="text" size="35" name="login"></h1><br>
-        <h2>パスワード
-          <input type="password" size="35" name="password"></h2><br>
-        <h3><input type="submit" value="ログイン"></h3>
+    <form action="login_comp.php" method="post">
+    <table>
+      <tr>
+        <td>
+          <h1>ログイン名</h1>
+        </td>
+        <td>
+          <input type="text"  name="login"><br>
+        </td>
+      </tr>
+      <tr>
+        <td>
+          <h2>パスワード</h2>
+        </td>
+        <td>
+          <input type="password"  name="password"><br>
+        </td>
+      </tr>
+    </table>
+          <h3><input type="submit" value="ログイン"></h3>
+    </form>
 </body>
 
 </html>
